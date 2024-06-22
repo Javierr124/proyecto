a72a79139b19c7901e17aea5da79717036cff12c
Javierr124 <71381660@certus.edu.pe>
2024-06-21 22:31:51 -0500

    calzado2

--- index.js
+++
@@ -1,6 +0,0 @@
-const btnCart = document.querySelector('.container-icon')
-const containercartproducts = document.querySelector('.container-cart-products')
-
-btnCart.addEventListener('click', () => {
-    containercartproducts.classList.toggle('hidden-cart')
-}) 
